87f7eaa5be148deb318caa3c3dfb1fc8184638dc
[muradjanazimov] <[muradjanazimov@gmail.com]>
2019-09-16 02:12:09 -0400

    contect page if almost done

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -12,44 +12,121 @@ import com.crm.qa.base.TestBase;
 
 public class ContactsPage extends TestBase {
 
-	
 	@FindBy(xpath = "//td[contains(text(),'Contacts')]")
 	WebElement contactsLabel;
-	
+
 	@FindBy(id = "first_name")
 	WebElement firstName;
-	
+
 	@FindBy(id = "surname")
 	WebElement lastName;
-	
+
 	@FindBy(name = "client_lookup")
 	WebElement company;
+
+	@FindBy(name = "nickname")
+	WebElement nickname;
+
+	@FindBy(name = "company_position")
+	WebElement companyPosition;
+
+	@FindBy(name = "department")
+	WebElement department;
+
+	@FindBy(name = "contact_lookup_sup")
+	WebElement supervisor;
+
+	@FindBy(name = "contact_lookup_ass")
+	WebElement assistant;
+
+	@FindBy(name = "contact_lookup_ref")
+	WebElement referredBy;
+
+	@FindBy(name = "phone")
+	WebElement phone;
+
+	@FindBy(name = "mobile")
+	WebElement mobile;
+
+	@FindBy(name = "home_phone")
+	WebElement home_phone;
+
+	@FindBy(name = "fax")
+	WebElement fax;
+
+	@FindBy(name = "email")
+	WebElement email;
 	
+	@FindBy(name = "im_id")
+	WebElement im_id;
+	
+	@FindBy(name = "skype_id")
+	WebElement skype_id;
+	
+	@FindBy(name = "identifier")
+	WebElement identifier;
+
 	@FindBy(xpath = "//input[@type='submit' and @value='Save']")
 	WebElement saveBtn;
-	
-	
+
 	public ContactsPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public boolean verifyContactsLebel() {
 		return contactsLabel.isDisplayed();
 	}
-	
+
 	public void selectContactsByName(String name) {
-		driver.findElement(By.xpath("//a[text()='"+name+"']//parent::td[@class='datalistrow']"
+		driver.findElement(By.xpath("//a[text()='" + name + "']//parent::td[@class='datalistrow']"
 				+ "//preceding-sibling::td[@class='datalistrow']//input[@name='contact_id']")).click();
 	}
-	
-	public void createNewContact(String title, String ftName, String ltName, String comp) {
+
+	public void createNewContact(String title, String ftName, String ltName, String comp, String nickName,
+			String companyposition, String suffix, String Department, String Supervisor, String Assistant,
+			String ReferredBy, String category, String status, String Phone, String Mobile, String Home_Phone,
+			String Fax, String Email, String Im_Id, String im_netowrk, String Skype_id, String Source, String Identifier) {
+
 		Select select = new Select(driver.findElement(By.xpath("//*[@name='title']")));
 		select.selectByVisibleText(title);
+		
 		firstName.sendKeys(ftName);
 		lastName.sendKeys(ltName);
 		company.sendKeys(comp);
+		nickname.sendKeys(nickName);
+		companyPosition.sendKeys(companyposition);
+		
+		select = new Select(driver.findElement(By.xpath("//*[@name='suffix']")));
+		select.selectByVisibleText(suffix);
+		
+		department.sendKeys(Department);
+		supervisor.sendKeys(Supervisor);
+		assistant.sendKeys(Assistant);
+		referredBy.sendKeys(ReferredBy);
+		
+		select = new Select(driver.findElement(By.xpath("//*[@name='category']")));
+		select.selectByVisibleText(category);
+		select = new Select(driver.findElement(By.xpath("//*[@name='status']")));
+		select.selectByVisibleText(status);
+		
+		phone.sendKeys(Phone);
+		mobile.sendKeys(Mobile);
+		home_phone.sendKeys(Home_Phone);
+		fax.sendKeys(Fax);
+		email.sendKeys(Email);
+		im_id.sendKeys(Im_Id);
+		
+		select = new Select(driver.findElement(By.xpath("//*[@name='im_netowrk']")));
+		select.selectByVisibleText(im_netowrk);
+		
+		skype_id.sendKeys(Skype_id);
+		
+		select = new Select(driver.findElement(By.xpath("//*[@name='source']")));
+		select.selectByVisibleText(Source);
+		
+		identifier.sendKeys(Identifier);
+		
 		saveBtn.click();
 	}
-	
 
 }
--- src/test/java/com/crm/qa/testcases/ContactPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactPageTest.java
@@ -19,9 +19,9 @@ public class ContactPageTest extends TestBase {
 	HomePage homePage;
 	TestUtil testUtil;
 	ContactsPage contactsPage;
-	//CalendarPage calendarPage;
-	
-	String sheetName="contacts";
+	// CalendarPage calendarPage;
+
+	String sheetName = "contacts";
 
 	public ContactPageTest() {
 		super();
@@ -33,7 +33,7 @@ public class ContactPageTest extends TestBase {
 		testUtil = new TestUtil();
 		loginPage = new LoginPage();
 		contactsPage = new ContactsPage();
-	//	calendarPage = new CalendarPage();
+		// calendarPage = new CalendarPage();
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 		testUtil.switchToFrame();
 		contactsPage = homePage.clickOnContactsLink();
@@ -55,21 +55,27 @@ public class ContactPageTest extends TestBase {
 		contactsPage.selectContactsByName("Iryna Azimova");
 		contactsPage.selectContactsByName("Micheal Jacson");
 	}
+
 	@DataProvider
 	public Object[][] getCRMTestData() {
 		Object data[][] = TestUtil.getTestData(sheetName);
 		return data;
-		//[] dinamtion
+		// [] dinamtion
 	}
-	
-	
-	@Test(priority=4, dataProvider="getCRMTestData")
-	public void validateCreateNewContact(String title, String firstName, String lastName, String company) {
+
+	@Test(priority = 4, dataProvider = "getCRMTestData")
+	public void validateCreateNewContact(String title, String firstName, String lastName, String company,
+			String nickname, String companyPosition, String suffix, String department, String supervisor,
+			String assistant, String referredBy, String category, String status, String phone, String mobile,
+			String home_phone, String fax, String email, String im_id, String im_netowrk, String skype_id,
+			String source, String identifier) {
+
 		homePage.clickOnNewContactLink();
-		//contactsPage.createNewContact("Miss", "Laura", "Lex", "Google");
-		contactsPage.createNewContact(title, firstName, lastName, company);
+		// contactsPage.createNewContact("Miss", "Laura", "Lex", "Google");
+		contactsPage.createNewContact(title, firstName, lastName, company, nickname, companyPosition, suffix,
+				department, supervisor, assistant, referredBy, category, status, phone, mobile, home_phone, fax, email,
+				im_id, im_netowrk, skype_id, source, identifier);
 	}
-	
 
 	@AfterMethod
 	public void tearDown() {
